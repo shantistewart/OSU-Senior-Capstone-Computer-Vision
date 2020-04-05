cc487898f7a1058a9b91a71f075e274bf9eef8fc
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-04 19:13:47 -0700

    Created initial project setup.

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/4/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include <stdio.h>
+
+
+int main() {
+    printf("Computer Vision Algorithms");
+	
+	printf("\n\n");
+    return 0;
+}
