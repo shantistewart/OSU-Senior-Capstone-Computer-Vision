f2d21eefc0434155f3e6fec0050e3c9d87bcd409
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-12 21:02:07 -0700

    Added sober struct.

--- Computer Vision Algorithms/Computer Vision Algorithms/structs_and_constants.h
+++ Computer Vision Algorithms/Computer Vision Algorithms/structs_and_constants.h
@@ -12,20 +12,17 @@
 
 
 
-// size of Gaussian kernel (must be odd!):
-#define KERNEL_SIZE 3
 // number of color channels:
 #define NUM_COLORS 3
 // number of rows of image pixels:
 #define NUM_ROWS 4
 // number of columns of image pixels:
 #define NUM_COLS 4
+// size of Gaussian kernel (must be odd!):
+#define KERNEL_SIZE 3
+// size of Sobel operators (must be odd!):
+#define SOBEL_SIZE 3
 
-
-// Gaussian kernel matrix:
-struct gaussian_kernel {
-	float kernel_matrix[KERNEL_SIZE][KERNEL_SIZE];
-};
 
 // RGB image pixel values:
 struct RGB_image {
@@ -35,6 +32,19 @@ struct RGB_image {
 // image (1 color channel) pixel values:
 struct image {
 	float pixels[NUM_ROWS][NUM_COLS];
+};
+
+// Gaussian kernel matrix:
+struct gaussian_kernel {
+	float kernel_matrix[KERNEL_SIZE][KERNEL_SIZE];
+};
+
+// Sobel operator matrices:
+struct sobel {
+	// horizontal gradient Sobel kernel (for vertical edges):
+	float sobel_horiz[SOBEL_SIZE][SOBEL_SIZE];
+	// % vertical gradient Sobel kernel (for horizontal edges):
+	float sobel_vert[SOBEL_SIZE][SOBEL_SIZE];
 };
 
 
