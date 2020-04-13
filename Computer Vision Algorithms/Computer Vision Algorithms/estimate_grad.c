97b45e0f85753c13d541a59cb58049f766eb2da9
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-12 20:55:12 -0700

    Created estimate_grad.h/.c files.

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/estimate_grad.c
@@ -0,0 +1,11 @@
+//
+//  estimate_grad.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/12/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include "estimate_grad.h"
+#include <stdio.h>
---
+++ Computer Vision Algorithms/Computer Vision Algorithms/estimate_grad.h
@@ -0,0 +1,18 @@
+//
+//  estimate_grad.h
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/12/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#ifndef estimate_grad_h
+#define estimate_grad_h
+
+
+#include "structs_and_constants.h"
+
+
+
+#endif /* estimate_grad_h */
