b5208a358c25d81146dfc951f9ca6925ac1db5d7
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-24 15:34:43 -0700

    Added non_max_suppress.h/.c

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/non_max_suppress.c
@@ -0,0 +1,11 @@
+//
+//  non_max_suppress.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/24/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include "non_max_suppress.h"
+#include <stdio.h>
---
+++ Computer Vision Algorithms/Computer Vision Algorithms/non_max_suppress.h
@@ -0,0 +1,18 @@
+//
+//  non_max_suppress.h
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/24/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#ifndef non_max_suppress_h
+#define non_max_suppress_h
+
+
+#include "structs_and_constants.h"
+
+
+
+#endif /* non_max_suppress_h */
