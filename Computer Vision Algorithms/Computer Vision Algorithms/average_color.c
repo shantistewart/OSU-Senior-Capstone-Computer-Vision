b0f8c9c8cce75ae68f4a7672ec1f246fbae55331
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-25 12:47:03 -0700

    Added average_color.h/.c

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/average_color.c
@@ -0,0 +1,11 @@
+//
+//  average_color.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/25/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include "average_color.h"
+#include <stdio.h>
---
+++ Computer Vision Algorithms/Computer Vision Algorithms/average_color.h
@@ -0,0 +1,18 @@
+//
+//  average_color.h
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/25/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#ifndef average_color_h
+#define average_color_h
+
+
+#include "structs_and_constants.h"
+
+
+
+#endif /* average_color_h */
