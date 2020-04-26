e9d4f1b82887fcc97c820016e072d4f183617dd1
Shanti Stewart <stewars2@oregonstate.edu>
2020-04-25 19:17:43 -0700

    Added find_edges.h/.c

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/find_edges.c
@@ -0,0 +1,11 @@
+//
+//  find_edges.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/25/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include "find_edges.h"
+#include <stdio.h>
---
+++ Computer Vision Algorithms/Computer Vision Algorithms/find_edges.h
@@ -0,0 +1,18 @@
+//
+//  find_edges.h
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 4/25/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#ifndef find_edges_h
+#define find_edges_h
+
+
+#include "structs_and_constants.h"
+
+
+
+#endif /* find_edges_h */
