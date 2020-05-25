a21e66b154a3858a1a9abd0d9e1e32aca138b5b7
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-24 20:50:07 -0700

    Added edge_detection..h/.c

---
+++ Computer Vision Algorithms/Computer Vision Algorithms/edge_detection.c
@@ -0,0 +1,10 @@
+//
+//  edge_detection.c
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 5/24/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#include "edge_detection.h"
---
+++ Computer Vision Algorithms/Computer Vision Algorithms/edge_detection.h
@@ -0,0 +1,19 @@
+//
+//  edge_detection.h
+//  Computer Vision Algorithms
+//
+//  Created by Shanti Stewart on 5/24/20.
+//  Copyright © 2020 Shanti Stewart. All rights reserved.
+//
+
+
+#ifndef edge_detection_h
+#define edge_detection_h
+
+
+#include <stdio.h>
+#include "structs_and_constants.h"
+
+
+
+#endif /* edge_detection_h */
