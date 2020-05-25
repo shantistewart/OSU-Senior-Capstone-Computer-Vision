2f142ba837699d73edb9972eb3bf8c343f0af493
Shanti Stewart <stewars2@oregonstate.edu>
2020-05-24 21:30:19 -0700

    Finished canny_edge_detection().

--- Computer Vision Algorithms/Computer Vision Algorithms/edge_detection.c
+++ Computer Vision Algorithms/Computer Vision Algorithms/edge_detection.c
@@ -8,7 +8,10 @@
 
 
 #include "edge_detection.h"
-#include "structs_and_constants.h"
+#include "blur_filter.h"
+#include "estimate_grad.h"
+#include "non_max_suppress.h"
+#include "find_edges.h"
 
 
 
@@ -17,21 +20,18 @@ Function Description: performs a modified version of the Canny edge detection al
 Inputs:
 	raw_image = raw RGB image
 		size: (NUM_COLORS, NUM_ROWS, NUM_COLS)
-	sigma = standard deviation of Gaussian kernel
-	suppress_length = (half) horizontal "suppression" length
-	low_thresh = lower gradient value threshold (0-255 pixel intensity)
-	high_thresh = higher gradient value threshold (0-255 pixel intensity)
-	vert_scan_length = vertical "scanning" length for edge continuity
-	horiz_scan_length = horizontal "scanning" length for edge continuity
-	min_edge_length = minimum edge length to keep
+	sigma = standard deviation of Gaussian kernel, in step 1
+	suppress_length = (half) horizontal "suppression" length, in step 3
+	low_thresh = lower gradient value threshold (0-255 pixel intensity), in step 4
+	high_thresh = higher gradient value threshold (0-255 pixel intensity), in step 4
+	vert_scan_length = vertical "scanning" length for edge continuity, in step 4
+	horiz_scan_length = horizontal "scanning" length for edge continuity, in step 4
+	min_edge_length = minimum edge length to keep, in step 4
 Outputs:
 	edges = list of edges in image, unsorted
 		type: struct edge_list
 */
 struct edge_list canny_edge_detection(float raw_image[NUM_COLORS][NUM_ROWS][NUM_COLS], float sigma, int suppress_length, int low_thresh, int high_thresh, int vert_scan_length, int horiz_scan_length, int min_edge_length) {
-	// array of edges in image:
-	struct edge_list edges;
-	
 	// wrap raw image values into a RGB_image struct:
 	struct RGB_image raw_pic;
 	for (int color=0; color<NUM_COLORS; color++) {
@@ -41,6 +41,21 @@ struct edge_list canny_edge_detection(float raw_image[NUM_COLORS][NUM_ROWS][NUM_
 			}
 		}
 	}
+	
+	
+	// STEP 1: GAUSSIAN BLUR FILTER
+	struct RGB_image blur_pic = blur_filter(raw_pic, sigma);
+	
+	// STEP 2: GRADIENT ESTIMATION
+	// to normalize Sobel operators:
+	int norm = 1;
+	struct image grads = estimate_grad(blur_pic, norm);
+	
+	// STEP 3: HORIZONTAL NON-MAXIMAL SUPPRESSION
+	struct image suppress_grads = non_max_suppress(grads, suppress_length);
+	
+	// STEP 4: LONG VERTICAL EDGE DETERMINATION
+	struct edge_list edges = find_edges(suppress_grads, low_thresh, high_thresh, vert_scan_length, horiz_scan_length, min_edge_length);
 	
 	
 	return edges;
